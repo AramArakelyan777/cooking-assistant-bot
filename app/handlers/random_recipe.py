fd6fa342c0066dd21c8fc79ff8e50f28f87ec1b0
Aram Arakelyan <aram777arakelyan@gmail.com>
2025-02-21 19:03:35 +0400

    enhanced readability

--- app/handlers/random_recipe.py
+++ app/handlers/random_recipe.py
@@ -8,7 +8,7 @@ config = dotenv_values(".env")
 logger = AsyncLoggingMiddleware()
 
 
-async def get_random_recipe() -> dict:
+async def get_random_recipe() -> dict | None:
     """Fetches and returns a random recipe using an API, handles possible errors."""
 
     try:
@@ -18,23 +18,23 @@ async def get_random_recipe() -> dict:
             random_recipe = await make_request(url=recipe_api)
         else:
             await logger.log(level="error", message="The recipe API is invalid.")
-            return {}
+            return None
 
         if random_recipe and random_recipe.get("meals") and isinstance(random_recipe["meals"], list) and len(random_recipe["meals"]) > 0:
             return random_recipe["meals"][0]
         else:
             await logger.log(level="error",
                              message="API returned no meals or invalid structure.")
-            return {}
+            return None
 
     except ClientError as e:
         await logger.log(level="error", message=f"Network error: {e}")
-        return {}
+        return None
 
     except KeyError as e:
         await logger.log(level="error", message=f"Missing configuration key: {e}")
-        return {}
+        return None
 
     except Exception as e:
         await logger.log(level="error", message=f"Unexpected error: {e}")
-        return {}
+        return None
