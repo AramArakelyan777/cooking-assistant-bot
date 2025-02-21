06e718e3f4e924a316868e8aa3e6f4e64b56a532
Aram Arakelyan <aram777arakelyan@gmail.com>
2025-02-21 19:28:32 +0400

    some changes

--- app/handlers/http/make_request.py
+++ app/handlers/http/make_request.py
@@ -1,21 +1,7 @@
-from handlers.http.session_manager import session
+import aiohttp
 
 
-async def make_request(url: str, params: dict = {}) -> dict:
-    """
-    Asynchronously makes an HTTP GET request to the specified URL with optional query parameters.
-
-    Args:
-        url (str): The URL to send the GET request to.
-        params (dict, optional): A dictionary of query parameters to include in the request. Defaults to an empty dictionary.
-
-    Returns:
-        dict: The JSON response from the server as a dictionary.
-
-    Raises:
-        aiohttp.ClientError: If an error occurs during the request.
-        aiohttp.ClientResponseError: If the response contains an HTTP error status.
-    """
-
-    async with session.get(url=url, params=params) as res:
-        return await res.json()
+async def make_request(url: str, params={}):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params) as res:
+            return await res.json()
--- app/middlewares/logging_middleware.py
+++ app/middlewares/logging_middleware.py
@@ -1,6 +1,9 @@
+import os
+from datetime import datetime
 from aiogram import BaseMiddleware
 from aiogram.types import Update
 from aiologger import Logger
+from aiologger.handlers.files import AsyncFileHandler
 
 
 class AsyncLoggingMiddleware(BaseMiddleware):
@@ -13,28 +16,51 @@ class AsyncLoggingMiddleware(BaseMiddleware):
         return cls._instance
 
     def _init_logger(self):
-        self.async_logger = Logger.with_default_handlers(
-            name="async_bot_logger")
+        log_dir = os.path.abspath("app/data/logs")
+        os.makedirs(log_dir, exist_ok=True)
+
+        self.debug_logger = Logger.with_default_handlers(name="debug_logger")
+        self.info_logger = Logger.with_default_handlers(name="info_logger")
+        self.error_logger = Logger.with_default_handlers(name="error_logger")
+
+        self.debug_logger.add_handler(
+            AsyncFileHandler(os.path.join(log_dir, "debug.log")))
+        self.info_logger.add_handler(
+            AsyncFileHandler(os.path.join(log_dir, "info.log")))
+        self.error_logger.add_handler(
+            AsyncFileHandler(os.path.join(log_dir, "error.log")))
 
     async def __call__(self, handler, event, data):
         if isinstance(event, Update):
-            await self.async_logger.debug(f"Received update: {event}")
+            await self.debug_logger.debug(self._format_message("DEBUG", f"Received update: {event}"))
 
         try:
             return await handler(event, data)
 
         except Exception as e:
-            await self.async_logger.error(f"Error while processing event: {e}", exc_info=True)
+            await self.error_logger.error(self._format_message("ERROR", f"Error while processing event: {e}"), exc_info=True)
             raise e
 
     async def log(self, level: str, message: str):
+        """Logs a message at a specific level asynchronously.
+        Args:
+            level (str): Logging level (debug, info, error).
+            message (str): Logging message.
+        """
+
         level = level.lower()
+        formatted_message = self._format_message(level.upper(), message)
 
         if level == "debug":
-            await self.async_logger.debug(message)
+            await self.debug_logger.debug(formatted_message)
         elif level == "info":
-            await self.async_logger.info(message)
+            await self.info_logger.info(formatted_message)
         elif level == "error":
-            await self.async_logger.error(message)
+            await self.error_logger.error(formatted_message)
         else:
-            await self.async_logger.warning(f"Unknown log level: {level} - {message}")
+            await self.error_logger.warning(self._format_message("WARNING", f"Unknown log level: {level} - {message}"))
+
+    def _format_message(self, level: str, message: str) -> str:
+        """Formats the log message to include logging date, level and message."""
+
+        return f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S,%f")[:-3]} [{level}] {message}"
